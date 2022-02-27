3f73975069d565807951dae1e137e11b08443cd1
mehdi-nait <mehdi.nait.hammou1@gmail.com>
2022-02-27 19:24:45 +0100

    working MVP, code needs refactoring

--- src/Main.ino
+++ src/Main.ino
@@ -82,7 +82,153 @@ void setup() {
 
 void loop() {
   //int label_id;
-  float aX, aY, aZ, gX, gY, gZ;
+  #include <ArduinoBLE.h>
+
+#include <Arduino_LSM9DS1.h>
+
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/micro/all_ops_resolver.h>
+#include <tensorflow/lite/micro/micro_error_reporter.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/version.h>
+
+#include "model.h"
+
+const float accelerationThreshold = 1.5; // threshold of significant in G's
+const int numSamples = 50;
+
+int samplesRead = numSamples;
+
+// global variables used for TensorFlow Lite (Micro)
+tflite::MicroErrorReporter tflErrorReporter;
+
+// pull in all the TFLM ops, you can remove this line and
+// only pull in the TFLM ops you need, if would like to reduce
+// the compiled size of the sketch.
+tflite::AllOpsResolver tflOpsResolver;
+
+const tflite::Model* tflModel = nullptr;
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+// Create a static memory buffer for TFLM, the size may need to
+// be adjusted based on the model you are using
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
+
+// array to map gesture index to a name
+const char* GESTURES[] = {
+  "Accelerate",
+  "Slow",
+  "Stop"
+};
+
+#define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  
+  // initialize the BLE hardware
+  BLE.begin();
+
+  Serial.println("GESTURE RECOGNITION - ENSIM 2022");
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+
+  Serial.println();
+
+
+  tflModel = tflite::GetModel(model);
+  if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
+    Serial.println("Model schema mismatch!");
+    while (1);
+  }
+
+  // Create an interpreter to run the model
+  tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
+
+  // Allocate memory for the model's input and output tensors
+  tflInterpreter->AllocateTensors();
+
+  // Get pointers for the model's input and output tensors
+  tflInputTensor = tflInterpreter->input(0);
+  tflOutputTensor = tflInterpreter->output(0);
+
+  // start scanning for peripherals
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+}
+
+void loop() {
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    if (peripheral.localName() != "LED") {
+      return;
+    }
+
+    // stop scanning
+    BLE.stopScan();
+
+    Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+
+  // retrieve the LED characteristic
+  BLECharacteristic ledCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
+
+  if (!ledCharacteristic) {
+    Serial.println("Peripheral does not have LED characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!ledCharacteristic.canWrite()) {
+    Serial.println("Peripheral does not have a writable LED characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+
+  while (peripheral.connected()) 
+  {
+    // while the peripheral is connected
+
+    float aX, aY, aZ, gX, gY, gZ;
 
   // wait for significant motion
   while (samplesRead == numSamples) {
@@ -132,15 +278,53 @@ void loop() {
         }
 
         // Loop through the output tensor values from the model
+        float max = 0;
+        int index = 1;
         for (int i = 0; i < NUM_GESTURES; i++) {
           Serial.print(GESTURES[i]);
           Serial.print(": ");
           Serial.println(tflOutputTensor->data.f[i], 6);
-          
+          if(tflOutputTensor->data.f[i]>max){
+            index = i;
+            max = tflOutputTensor->data.f[i];
+          }
         }
+        Serial.print("Max : ");
+        Serial.print(max);
         Serial.println();
+        Serial.print("index :");
+        Serial.print(index);
+        Serial.println("\n");
+
+        if (index==0) {
+        // button is pressed, write 0x01 to turn the LED on
+          ledCharacteristic.writeValue((byte)0x01);
+          delay(5000);
+        } 
+        else {
+          if(index==1){
+          // button is released, write 0x00 to turn the LED off
+          ledCharacteristic.writeValue((byte)0x00);
+          delay(5000);
+          }
+          else{
+            ledCharacteristic.writeValue((byte)0x10);
+            delay(5000);
+         }
+        }
+        
       }
     }
   }
+  }
+
+  Serial.println("Peripheral disconnected");
+
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  }
+}
+
+
 
 }
